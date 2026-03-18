6fe912665d456fbdb83959ba1d50e00d111dc9c7
yuliaslatvitskaya <yuli.slat@yandex.ru>
2026-03-18 20:42:43 +0100

    add scripts for animation ground and m&m

--- assets/Scripts/MMController.ts
+++ assets/Scripts/MMController.ts
@@ -1,35 +1,48 @@
-import { _decorator, Component, Node , Vec3 , tween} from 'cc';
+import { _decorator, Component, Vec3, tween } from 'cc';
 const { ccclass } = _decorator;
 
 @ccclass('MMController')
 export class MMController extends Component {
+
     start() {
-        this.startFlight();
+        this.loopFlight();
     }
 
-    startFlight() {
+    loopFlight() {
         const start = this.node.position.clone();
 
-        const end = new Vec3(start.x + 500, start.y, 0);
-        const control = new Vec3(start.x + 250, start.y + 200, 0);
+        const right = new Vec3(start.x + 150, start.y, 0);
 
+        const upControl = new Vec3(start.x + 75, start.y + 120, 0);
+        const downControl = new Vec3(start.x + 75, start.y - 120, 0);
+
+        this.moveBezier(start, upControl, right, () => {
+
+            this.moveBezier(right, downControl, start, () => {
+
+                this.loopFlight();
+            });
+        });
+    }
+
+    moveBezier(p0: Vec3, p1: Vec3, p2: Vec3, callback?: Function) {
         let t = { value: 0 };
 
         tween(t)
-            .to(2, { value: 1 }, {
+            .to(1.2, { value: 1 }, {
                 onUpdate: () => {
-                    const x = (1 - t.value) * (1 - t.value) * start.x +
-                        2 * (1 - t.value) * t.value * control.x +
-                        t.value * t.value * end.x;
+                    const x = (1 - t.value) * (1 - t.value) * p0.x +
+                        2 * (1 - t.value) * t.value * p1.x +
+                        t.value * t.value * p2.x;
 
-                    const y = (1 - t.value) * (1 - t.value) * start.y +
-                        2 * (1 - t.value) * t.value * control.y +
-                        t.value * t.value * end.y;
+                    const y = (1 - t.value) * (1 - t.value) * p0.y +
+                        2 * (1 - t.value) * t.value * p1.y +
+                        t.value * t.value * p2.y;
 
                     this.node.setPosition(new Vec3(x, y, 0));
                 }
             })
+            .call(() => callback && callback())
             .start();
     }
 }
-
